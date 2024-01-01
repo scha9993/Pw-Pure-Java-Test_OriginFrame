3a44fcc928fa1286281404f74967bed35d33d610
Sanduni Chamika <sanduni.chamika@syscolab.com>
2024-01-01 23:31:42 +0530

    headed mode

--- src/test/java/HybrisTest.java
+++ src/test/java/HybrisTest.java
@@ -7,30 +7,26 @@ public class HybrisTest {
     private void PaymentPopUpTest(){
         //UN: paypagetest7@qa.com
         //PW: Welcome123
-        try (Playwright playwright = Playwright.create()) {
-            BrowserType browserType = playwright.chromium();
-            Browser browser = browserType.launch();
-            BrowserContext browserContext = browser.newContext();
-            Page page = browserContext.newPage();
-            page.navigate("https://www-s3.brake.co.uk/");
-            Locator btnUser = page.locator("//button[@aria-label='Sign in']");
-            page.click(String.valueOf(btnUser));
-            //TODO navigate to update card
-            Locator btnUpdateCard = page.getByText("Update");
-            btnUpdateCard.click();
-            Locator iFramePayment = (Locator) page.frameLocator("#paymentIframe");
-            Locator txtFldCardNo = iFramePayment.getByPlaceholder("0000 0000 0000 0000");
-            Locator txtFldCardHolder = iFramePayment.getByPlaceholder("John Doe");
-            Locator txtFldExpiry = iFramePayment.getByPlaceholder("MM/YY");
-            Locator txtFldCVV = iFramePayment.getByPlaceholder("123");
-            Locator btnConfirm = iFramePayment.locator("//input[@value='Confirm']");
-            Locator txtFldSecretCode = iFramePayment.getByPlaceholder("Enter Code here");
-            txtFldCardNo.fill("5434600000000044");
-            txtFldCardHolder.fill("Brakes test1");
-            txtFldExpiry.fill("12/25");
-            txtFldCVV.fill("123");
-            btnConfirm.click();
-            txtFldSecretCode.fill("555");
-        }
+        Page page = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)).newContext().newPage();
+        page.navigate("https://www-s3.brake.co.uk/");
+        Locator btnUser = page.locator("//button[@aria-label='login']");
+        page.click(String.valueOf(btnUser));
+        //TODO navigate to update card
+        Locator btnUpdateCard = page.getByText("Update");
+        btnUpdateCard.click();
+        Locator iFramePayment = (Locator) page.frameLocator("#paymentIframe");
+        Locator txtFldCardNo = iFramePayment.getByPlaceholder("0000 0000 0000 0000");
+        Locator txtFldCardHolder = iFramePayment.getByPlaceholder("John Doe");
+        Locator txtFldExpiry = iFramePayment.getByPlaceholder("MM/YY");
+        Locator txtFldCVV = iFramePayment.getByPlaceholder("123");
+        Locator btnConfirm = iFramePayment.locator("//input[@value='Confirm']");
+        Locator txtFldSecretCode = iFramePayment.getByPlaceholder("Enter Code here");
+        txtFldCardNo.fill("5434600000000044");
+        txtFldCardHolder.fill("Brakes test1");
+        txtFldExpiry.fill("12/25");
+        txtFldCVV.fill("123");
+        btnConfirm.click();
+        txtFldSecretCode.fill("555");
+
     }
 }
